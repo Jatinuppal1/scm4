75fdba3aa40ab6cedf66065b4db18338c43f9493
Jatin Uppal <jatinuppal89@gmail.com>
2019-03-07 11:01:58 +0530

    parametrized constructor check

---
+++ file1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class sample
+{
+	int a,b;
+	public:
+	sample(int a, int b)
+	{
+		(*this).a=a;
+		(*this).b=b;
+	}
+	void print()
+	{
+		cout<<"\na = "<<a;
+		cout<<"\nb = "<<b;
+	}
+};
+int main()
+{ 
+	sample s1(10,20);
+	s1.print();
+	return 0;
+}
